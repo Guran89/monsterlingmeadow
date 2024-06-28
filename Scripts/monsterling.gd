d363333e3998d7663ce043727044ecbce8707550
Kristoffer Warnberg <kristoffer.warnberg@gmail.com>
2024-06-28 22:42:15 +0200

    Add  eat animations

--- Scripts/monsterling.gd
+++ Scripts/monsterling.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var _feed_area = $FeedArea
 @onready var _hunger = $Hunger
+@onready var eat_timer = $mouths/EatTimer
 
 # References to the AnimatedSprite2D nodes
 @onready var arm_sprite = $arms
@@ -11,18 +12,25 @@ extends Node2D
 @onready var mouth_sprite = $mouths
 @onready var torso_sprite = $torsos
 
+var _torso
+var _head
+var _arms
+var _legs
+var _mouth
+var _eyes
+
 func _ready():
 	Events.food_item_dropped.connect(_on_food_item_dropped)
 	Events.time_changed.connect(_on_time_changed)
 
 	randomize()
 
-	var _torso = get_random(Data.torsos.values())
-	var _head = get_random(Data.heads.values())
-	var _arms = get_random(Data.arms.values())
-	var _legs = get_random(Data.legs.values())
-	var _mouth = get_random(Data.mouths.values())
-	var _eyes = get_random(Data.eyes.values())
+	_torso = get_random(Data.torsos.values())
+	_head = get_random(Data.heads.values())
+	_arms = get_random(Data.arms.values())
+	_legs = get_random(Data.legs.values())
+	_mouth = get_random(Data.mouths.values())
+	_eyes = get_random(Data.eyes.values())
 
 	head_sprite.sprite_frames = _head.idle_animation
 	mouth_sprite.sprite_frames = _mouth.idle_animation
@@ -43,7 +51,14 @@ func _on_feed_area_input_event(viewport, event, shape_idx):
 func _on_food_item_dropped(food_item):
 	if _feed_area.hover():
 		_hunger.change_hunger(food_item.get_food().nutrition)
+		mouth_sprite.sprite_frames = _mouth.eat_animation
+		mouth_sprite.play()
 
 func _on_time_changed(time):
 	if time == Data.times.DAY || time == Data.times.NIGHT:
 		_hunger.change_hunger(Constants.FOOD_LOSS_PER_DAY)
+
+
+func _on_mouths_animation_finished():
+	mouth_sprite.sprite_frames = _mouth.idle_animation
+	mouth_sprite.play()
