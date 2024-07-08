0252cbf47d1df5ba946c4fb3a121bf24ef9d9bb6
SoundChemist <kiddia1@hotmail.com>
2024-07-08 16:17:26 +0200

    fixed music switches with fades

--- Scripts/background.gd
+++ Scripts/background.gd
@@ -10,18 +10,31 @@ var fade_parameter: float
 
 func _ready():
 	Events.time_changed.connect(_on_time_changed)
-	#FMODRuntime.play_one_shot(event)
-	#play day music
 	dayMusicInstance = FMODRuntime.create_instance(dayMusic)
+	#dayMusicInstance.start()
+	nightMusicInstance = FMODRuntime.create_instance(nightMusic)
+
+# Start both music instances paused
 	dayMusicInstance.start()
+	dayMusicInstance.set_paused(true)
+	nightMusicInstance.start()
+	nightMusicInstance.set_paused(true)
 
 func _on_time_changed(time):
 	texture = time.texture
-	
-	#fade day music
-	fade_parameter = 1.0 #set fade parameter to 1
-	dayMusicInstance.set_parameter_by_name("Fade", fade_parameter, false)
-	
-	#play night music
-	#nightMusicInstance = FMODRuntime.create_instance(nightMusic)
-	#nightMusicInstance.start()
+
+func _process(delta):
+	if Data.time == 0:
+		# Fade out night music, fade in day music
+		fade_parameter = min(fade_parameter + delta, 1.0)
+		dayMusicInstance.set_paused(false)
+		nightMusicInstance.set_paused(false)
+		dayMusicInstance.set_parameter_by_name("Fade", fade_parameter, false)
+		nightMusicInstance.set_parameter_by_name("Fade", 1.0 - fade_parameter, false)
+	else:
+		# Fade out day music, fade in night music
+		fade_parameter = max(fade_parameter - delta, 0.0)
+		dayMusicInstance.set_paused(false)
+		nightMusicInstance.set_paused(false)
+		dayMusicInstance.set_parameter_by_name("Fade", fade_parameter, false)
+		nightMusicInstance.set_parameter_by_name("Fade", 1.0 - fade_parameter, false)
